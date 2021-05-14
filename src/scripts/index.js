b98e0de1a7e66f04a169f47339e9d567ec68a6e3
Sophie Milash <sophie.milash@yandex.ru>
2021-05-14 13:42:04 +0200

    feat: configure webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,4 @@
+import '../pages/index.css';
 import { initialCards, validationConfig, editPopupConfig, addPopupConfig, imagePopupConfig } from './utils/constants.js';
 import Card from './components/Card.js';
 import FormValidator from './components/FormValidator.js';
